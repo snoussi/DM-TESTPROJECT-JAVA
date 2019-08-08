97e6a52960fdc245994567ad8e9df3609409858c
snoussi <rachid@redhat.com>
2019-08-08 02:11:00 +0200

    fixed

--- src/main/java/com/total/anac/sample/DroolsRuleEngineExample.java
+++ src/main/java/com/total/anac/sample/DroolsRuleEngineExample.java
@@ -1,11 +1,11 @@
 package com.total.anac.sample;
 
+import com.total.anac.model.Sample;
+import org.kie.api.KieBase;
 import org.kie.api.KieServices;
 import org.kie.api.builder.ReleaseId;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-
-import com.total.anac.model.Sample;
 
 
 /**
@@ -16,9 +16,11 @@ public class DroolsRuleEngineExample {
 	public static final void main(String[] args) {
 		try{
 			KieServices ks = KieServices.Factory.get();
-			ReleaseId releaseId = ks.newReleaseId("com.myspace", "ProjetdeTest", "1.0.0-SNAPSHOT");
+			ReleaseId releaseId = ks.newReleaseId("com.total.anac", "ProjetDeTest", "1.0.0-SNAPSHOT");
 			KieContainer kieContainer = ks.newKieContainer(releaseId);
-			KieSession kieSession = kieContainer.newKieSession();
+//			KieSession kieSession = kieContainer.newKieSession();
+			KieBase kieBase = kieContainer.getKieBase();
+			KieSession kieSession = kieBase.newKieSession();
 			Sample sample = new Sample("green", 40.00, 0, 0);
 			kieSession.insert(sample);
 			System.out.println("************* Fire Rules **************");
@@ -27,8 +29,13 @@ public class DroolsRuleEngineExample {
 			 * test_group_1 : fire the rules with using the option no-loop in all rules
 			 * test_group_2 : fire the rules with using the option lock-on-active in the first rule
 			 * test_group_3 : fire the rules with using the option lock-on-active in all rules
+			 * test_group_4 : simple rule inference, no priority, no no-loop, no lock-on-active, last rule shouldn't
+			 * update the working memory, but only the fact attributes
+			 * test_group_5 : an example with a guided decision table + usage of a Metadata to display the rule order
 			 */
-			kieSession.getAgenda().getAgendaGroup("test_group_1").setFocus();
+
+
+			kieSession.getAgenda().getAgendaGroup("test_group_5").setFocus();
 			kieSession.fireAllRules();
 			kieSession.dispose();
 			System.out.println(sample);
